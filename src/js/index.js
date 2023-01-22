034e240f7a7f8cbe4f2970527cfea953137eebd4
black0utt <wesleysv61@gmail.com>
2023-01-22 17:35:12 -0300

    iniciando a implementação dos scripts

---
+++ src/js/index.js
@@ -0,0 +1,31 @@
+/*
+
+OBJETIVO 1 - quando o usuário clicar no botão 
+de veja o trailer, devemos abrir a modal
+com o video do trailer
+
+    - passo 1 - pegar o elemento que representa
+    o botao na tela usando o js
+
+    - passo 2 - dar um jeito de identificar
+    quando o usuário clicar no botão
+
+    - passo 3 - dar um jeito de pegar o elemento
+    da modal no js
+    
+    - passo 4 - abrir a modal na tela
+
+objetivo 2 - quando o usuário clicar no x
+devemos fechar a modal
+
+    - passo 1 - pegar o elemento de fechar
+    o modal usando js
+
+    - passo 2 - identificar quando o usuário
+    clicar no X
+
+    -passo 3 fechar a modal
+
+*/ 
+
+console.log(document);
